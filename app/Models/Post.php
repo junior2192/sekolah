88fcd69ebc813c74d878fdfe03252b0d167bc42a
opik211192 <opiktaofik21@gmail.com>
2022-12-22 20:43:55 +0700

    sekolah

---
+++ app/Models/Post.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function category()
+    {
+
+    }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+
+            get: fn ($value) => asset('/storage/posts/' .$value),
+        );
+    }
+}
---
+++ database/seeders/PermissionTableSeeder.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class PermissionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //permission post
+        Permission::create(['name' => 'posts.index']);
+        Permission::create(['name' => 'posts.create']);
+        Permission::create(['name' => 'posts.edit']);
+        Permission::create(['name' => 'posts.delete']);
+
+        //permission tags
+        Permission::create(['name' => 'tags.index']);
+        Permission::create(['name' => 'tags.create']);
+        Permission::create(['name' => 'tags.edit']);
+        Permission::create(['name' => 'tags.delete']);
+
+        //permission categories
+        Permission::create(['name' => 'categories.index']);
+        Permission::create(['name' => 'categories.create']);
+        Permission::create(['name' => 'categories.edit']);
+        Permission::create(['name' => 'categories.delete']);
+
+        //permission events
+        Permission::create(['name' => 'events.index']);
+        Permission::create(['name' => 'events.create']);
+        Permission::create(['name' => 'events.edit']);
+        Permission::create(['name' => 'events.delete']);
+
+        //permission photos
+        Permission::create(['name' => 'photos.index']);
+        Permission::create(['name' => 'photos.create']);
+        Permission::create(['name' => 'photos.edit']);
+        Permission::create(['name' => 'photos.delete']);
+
+        //permission videos
+        Permission::create(['name' => 'videos.index']);
+        Permission::create(['name' => 'videos.create']);
+        Permission::create(['name' => 'videos.edit']);
+        Permission::create(['name' => 'videos.delete']);
+
+        //permission roles
+        Permission::create(['name' => 'roles.index']);
+        Permission::create(['name' => 'roles.create']);
+        Permission::create(['name' => 'roles.edit']);
+        Permission::create(['name' => 'roles.delete']);
+
+        //permission for permissions
+        Permission::create(['name' => 'permissions.index']);
+
+        //permission for users
+        Permission::create(['name' => 'users.index']);
+        Permission::create(['name' => 'users.create']);
+        Permission::create(['name' => 'users.edit']);
+        Permission::create(['name' => 'users.delete']);
+
+
+    }
+}
